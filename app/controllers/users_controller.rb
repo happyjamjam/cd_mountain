a500a77599145ad31aa62e9b822090a316667e64
i-chan <iwashiro2000@gmail.com>
2019-06-10 07:32:11 +0000

    pull

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+def show
+  	@user = current_user
+    @orders = current_user.orders.all
+    @favorites = current_user.favorites.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :encrypted_password, :postal_code, :address, :tel)
+  end
+
+  def correct_user
+  	user = User.find(params[:id])
+  	if current_user != user
+  		redirect_to user_path(current_user)
+    end
+  end
+end
