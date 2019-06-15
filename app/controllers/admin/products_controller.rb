06840d6adeb7b07626f75fc9714521ad65164be6
annaaida <annatakahashi813@gmail.com>
2019-06-15 07:42:35 +0000

    [Add] index page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,6 +8,11 @@ class Admin::ProductsController < Admin::ApplicationController
 
   def create
 
+  	@product = Product.new(product_params)
+
+  	@book.save
+    redirect_to admin_products(current_admin)
+
   end
 
   def index
@@ -18,16 +23,37 @@ class Admin::ProductsController < Admin::ApplicationController
 
   def edit
 
+  	@ = Product.find(params[:id])
+
   end
 
   def update
+
+    @cart = Product.find(params[:id])
+
+    if @cart.update(_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
 
   end
 
   def show
 
-  @product = Product.find(params[:id])
+    @product = Product.find(params[:id])
 
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:product_name, :label_id, :price, :genre_id, :stock, :sales_status)
   end
 
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,7 +12,7 @@ class CartsController < ApplicationController
   	@cart_item.user_id = current_user.id
 
   	@cart_item.save
-  	redirect_to carts_index_path(@cart)
+  	redirect_to carts_path(current_user)
 
   end
 
@@ -22,7 +22,7 @@ class CartsController < ApplicationController
   	@cart.user_id = current_user.id
 
   	@cart.update(cart_params)
-  	redirect_to carts_path(@cart)
+  	redirect_to carts_path(current_user)
 
   end
 
@@ -31,7 +31,7 @@ class CartsController < ApplicationController
   	cart = Cart.find(params[:id])
 
   	cart.destroy
-  	redirect_to carts_path(@cart)
+  	redirect_to carts_path(current_user)
 
   end
 
