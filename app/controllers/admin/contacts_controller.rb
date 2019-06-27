b27fae532c45680f7ec3aa2c2e23e2f6391c77ce
papipon <daaiisk0616@gmail.com>
2019-06-27 05:32:13 +0000

    フォーム作成しました

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -5,20 +5,26 @@ class Admin::ContactsController < Admin::ApplicationController
 
   def show
   	@contact = Contact.find(params[:id])
+    @newcontact = Contact.new
 
   end
 
   def update
 
-    contact = @contac
+    contact = @contact
     user = contact.user
     ContactMailer.send_when_admin_reply(user, contact).deliver
-
   end
 
   def destroy
   	contact = Contact.find(params[:id])
   	contact.destroy
+    redirect_to admin_contacts_path
+  end
+
+  def create
+    @contact = Contact.new.find(prams[:id])
+    @contact.save
     redirect_to admin_contacts_path
   end
 
