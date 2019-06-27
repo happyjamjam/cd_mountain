70de41e09b0f3cf00768dad2539fa4974173b096
annaaida <annatakahashi813@gmail.com>
2019-06-27 11:34:30 +0000

    メーラーテスト成功。問い合わせ、レビュー投稿者名修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,16 +1,11 @@
 class ContactMailer < ApplicationMailer
 
-<<<<<<< HEAD
   default from: 'CD MOUNTAIN 管理人 <annaaidamailer@gmail.com>'
-=======
-  default from: 'CD MOUNTAIN 管理人 <shunsuke8935@gmail.com>'
->>>>>>> master
   layout 'mailer'
 
   def send_when_admin_reply(user, contact)
   	@user = user
   	@answer = contact.reply_text
-	mail to:      @user.email
-         subject:'【CD MOUNTAIN】お問い合わせの回答。'
+	mail to: @user.email, subject:'【CD MOUNTAIN】お問い合わせの回答。'
   end
 end
